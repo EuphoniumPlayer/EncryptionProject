95a75faa2f74b6f88d9bf1efd757fac85df95b5a
Chase Manzella <chasewmanzella@gmail.com>
2026-04-28 09:47:34 -0500

    started on reading
    Started on creating the read method for file io. Also updated the FileException class to better be able to suit the needs of the program.

---
+++ src/FileException.java
@@ -0,0 +1,34 @@
+public class FileException extends Exception {
+    public static final int FILE_NOT_FOUND = 1;
+    public static final int DIRECTORY_NOT_FOUND = 2;
+    public static final int FILE_EXPLORER_ERROR = 3;
+
+    private String errorMessage;
+
+    public FileException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public FileException(String errorMessage, boolean state) {
+        if (state) {
+            this.errorMessage = "Unexpected error: " + errorMessage;
+        }
+    }
+
+    public FileException(int code) {
+        if (code == 1) { //File not found
+            this.errorMessage = "File not found.";
+        }
+        if (code == 2) { //Directory not found
+            this.errorMessage = "Directory not found.";
+        }
+        if (code == 3) {
+            this.errorMessage = "File Explorer error. Please try again.";
+        }
+    }
+
+    @Override
+    public String getMessage() {
+        return this.errorMessage;
+    }
+}
--- src/FileIO.java
+++ src/FileIO.java
@@ -1,17 +1,36 @@
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.*;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileIO {
     private String path;
-    private static String getFile() {
+    private static String getFilePath() throws FileException {
         JFileChooser chooser = new JFileChooser();
 
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Key Files (*.keys)", "keys");
         chooser.setFileFilter(filter);
 
         int approved = chooser.showOpenDialog(null);
-        File file = chooser.getSelectedFile();
-        return file.getAbsolutePath();
+        if (approved == JFileChooser.APPROVE_OPTION) {
+            try {
+                File file = chooser.getSelectedFile();
+                return file.getAbsolutePath();
+            } catch (Exception e) {
+                throw new FileException(e.getMessage(), true);
+            }
+        } else {
+            throw new FileException(FileException.FILE_EXPLORER_ERROR);
+        }
+    }
+
+    private static String[] readFile() {
+        int e=0,m=0,d=0;
+        String[] values = new String[3];
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(getFilePath()));
+        }
     }
 }
